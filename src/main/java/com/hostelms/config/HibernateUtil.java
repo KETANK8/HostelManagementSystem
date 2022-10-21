2757db7ccb04888631238cd9b8d3d009ae6151fb
KETANK8 <ketankumar126@gmail.com>
2022-10-21 12:16:35 +0530

    Commit

--- src/main/java/com/hostelms/config/HibernateUtil.java
+++ src/main/java/com/hostelms/config/HibernateUtil.java
@@ -13,39 +13,48 @@ import org.hibernate.cfg.Environment;
 
 public class HibernateUtil {
 
+	// creating session factory object
 	private static SessionFactory sesfac;
 
+	// getting logger in hibernateUtil class
 	static Logger log = Logger.getLogger(HibernateUtil.class);
 
 	public static SessionFactory getSessionFactory() {
 
+		// create a new session factory if not exist
 		if (sesfac == null) {
 
+			// setting properties of hibernate to connect with database
 			try {
 				Configuration config = new Configuration();
 				Properties pro = new Properties();
 
-				pro.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-				pro.put(Environment.URL, "jdbc:mysql://localhost:3306/hostel");
-				pro.put(Environment.USER, "root");
-				pro.put(Environment.PASS, "142307");
-				pro.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-				pro.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-				pro.put(Environment.HBM2DDL_AUTO, "update");
-				pro.put(Environment.SHOW_SQL, "false");
+				// setting hibernate properties to access database
+				pro.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver"); // getting mysql driver
+				pro.put(Environment.URL, "jdbc:mysql://localhost:3306/hostel"); // connecting with database
+				pro.put(Environment.USER, "root"); // user name of mysql server
+				pro.put(Environment.PASS, "142307"); // password of mysql server
+				pro.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect"); // setting mysql dialect
+				pro.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread"); // checking the session by no of execution
+																				// of threads
+				pro.put(Environment.HBM2DDL_AUTO, "update"); // creating or updating the table if already exist
+				pro.put(Environment.SHOW_SQL, "false"); // by indicating false we are not going to show sql process in
+														// our console
 
-				config.setProperties(pro);
-				config.addAnnotatedClass(User.class);
+				config.setProperties(pro); // configure properties that we created
+				config.addAnnotatedClass(User.class); // adding entity classes
 				config.addAnnotatedClass(Room.class);
 				sesfac = config.buildSessionFactory();
 
-			} catch (Exception e) {
+			} catch (Exception e) { // if any exception occur handaling the exception
 				log.info(e.getMessage());
 			}
 		}
 		return sesfac;
 	}
 
+	// getSession method to create a new session from sessionFactory pool
+	// everytime this method is called
 	public static Session getSession() {
 
 		return getSessionFactory().openSession();
--- src/main/java/com/hostelms/model/User.java
+++ src/main/java/com/hostelms/model/User.java
@@ -6,7 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,36 +18,22 @@ import lombok.ToString;
 public class User {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
-	
-	// Using Regular Expression
-	// RegEx To Check Validation
-	// Giving Condition to Set Unique UserName
 	@NotNull
 	private String userName;
-	
 	@NotNull
 	private String firstName;
 	@NotNull
 	private String lastName;
-	
-	// Using Regular Expression
-	// RegEx To Check Validation	
-	// Giving Condition to Set Contact Number
 	@NotNull
-	
 	private String userContact;
-
-	// Using Regular Expression
-	// RegEx To Check Validation	
-	// Giving Condition to Set Unique Password
 	@NotNull
 	private String userPassword;
 	private String userAddress;
 	private String userRole;
 	private int userRent;
-	
+
 	@ManyToOne
 	private Room userRoom;
 }
